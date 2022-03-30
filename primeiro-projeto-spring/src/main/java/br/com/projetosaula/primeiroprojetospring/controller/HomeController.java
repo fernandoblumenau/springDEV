dceca77af8469ded9eb2b0a7eb07f01a10efcdb1
Fernando Rodrigues <fernando_criciuma@hotmail.com>
2022-03-29 22:01:49 -0300

    first commit

---
+++ primeiro-projeto-spring/src/main/java/br/com/projetosaula/primeiroprojetospring/controller/HomeController.java
@@ -0,0 +1,18 @@
+package br.com.projetosaula.primeiroprojetospring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController()
+@RequestMapping("/")
+public class HomeController {
+
+	public HomeController() {
+		
+		System.out.println("Funcionou");
+		
+	}
+
+}

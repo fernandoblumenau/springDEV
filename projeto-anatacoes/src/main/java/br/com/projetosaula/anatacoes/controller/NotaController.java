1eec0e90d8baea540b8e4ddcaac59709ef80a1ce
Fernando Rodrigues <moc@proway.treina>
2022-05-02 21:51:52 -0300

    02/05/2022

--- projeto-anatacoes/src/main/java/br/com/projetosaula/anatacoes/controller/NotaController.java
+++ projeto-anatacoes/src/main/java/br/com/projetosaula/anatacoes/controller/NotaController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projetosaula.anatacoes.data.dto.NotaDTO;
@@ -48,5 +50,11 @@ public class NotaController {
 		return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
 		
 	}
+	
+	@PutMapping
+	public ResponseEntity<NotaDTO> update(@RequestBody NotaDTO nota) throws Exception{
+		return new ResponseEntity<> (service.update(nota), HttpStatus.CREATED);		
+	}
+	
 	
 }
